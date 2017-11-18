ee988426faf4cea3b8323a98e7b46ba05ed87f7c
Sten Liao <callmelei@gmail.com>
2017-11-17 23:08:00 -0800

    deploy configuration

--- courseRegistration/settings.py
+++ courseRegistration/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '@#x9t_v*v*apue)-p3o(r+bngx+37ka$zxn%xevxb1-9!mzr^y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
@@ -80,5 +80,19 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+
+DEBUG = False
 
 STATIC_URL = '/static/'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
